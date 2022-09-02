f5623915476a1d42588fa063b9c09109df502cc1
noah-clements <noahclements@gmail.com>
2022-09-02 02:48:59 -0400

    freeze reqs add Procfile & convert to postgres

--- blog-with-users/bloggr/__init__.py
+++ blog-with-users/bloggr/__init__.py
@@ -18,20 +18,28 @@ def create_app(test_config=None):
     # initialize Gravatar (images for users)
     gravatar.init_app(app)
 
-    ##CONNECT TO DB
+    ##Default dev DB configuration
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    if test_config is None:
+        # load the instance config, if it exists, when not testing
+        app.config.from_file(".project_config", load=toml.load, silent=True)
+        # get production setup from os
+        app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
+        app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
+    
+    else:
+        # load the test config if passed in
+        app.config.from_mapping(test_config)
+
+
+    ##CONNECT TO DB
     db.init_app(app)
     # Register init-db cli command from the model (flask tutorial)
     from . import models
     models.init_app(app)
 
-    if test_config is None:
-        # load the instance config, if it exists, when not testing
-        app.config.from_file(".project_config", load=toml.load, silent=True)
-    else:
-        # load the test config if passed in
-        app.config.from_mapping(test_config)
 
     # ensure the instance folder exists
     try:
