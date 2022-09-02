1352e8f8745b1a5eadfc45ba8e2c21ef22358a6b
noah-clements <noahclements@gmail.com>
2022-09-01 22:03:38 -0400

    day 69 refactor blog move to bloggr folder

--- blog-with-users/bloggr/__init__.py
+++ blog-with-users/bloggr/__init__.py
@@ -1,14 +1,25 @@
 import os
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 import toml
+
+db = SQLAlchemy()
 
 def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True )
     app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
 
+    ##CONNECT TO DB
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    db.init_app(app)
+    # Register init-db cli command from the model (flask tutorial)
+    from . import models
+    models.init_app(app)
+
     if test_config is None:
         # load the instance config, if it exists, when not testing
-        app.config.from_file(".project_config", load=toml.load)
+        app.config.from_file(".project_config", load=toml.load, silent=True)
     else:
         # load the test config if passed in
         app.config.from_mapping(test_config)
--- blog-with-users/bloggr/db.py
+++
@@ -1,4 +0,0 @@
-from flask import current_app, g
-from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.orm import relationship
-
