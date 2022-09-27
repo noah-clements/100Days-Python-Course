21e1c78881f150fae26e5e6272702acdc27d3034
noah-clements <noahclements@gmail.com>
2022-09-27 02:21:14 -0400

    Middle of Day 75 - first commit

---
+++ test_repr.py
@@ -0,0 +1,14 @@
+import math
+
+class Vector:
+    def __init__(self, x=0, y=0):
+        self.x = x
+        self.y = y
+
+    def __repr__(self):
+        return f'Vector({self.x!r}, {self.y!r})'
+
+
+
+v = Vector(1.5,"2")
+print(v)
